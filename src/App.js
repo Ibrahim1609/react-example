8e64d33363d8f8bf8917e6e933f50855f8d86139
mohamadibrahim <mohamadibrahim@hakunamatata.in>
2023-11-01 17:13:25 +0530

    Formatting the files

--- src/App.js
+++ src/App.js
@@ -1,150 +1,166 @@
-import React, { useState } from "react";
-import "./App.css";
-import "./index.css";
+import React, { useState } from 'react'
+import './App.css'
+import './index.css'
 
-function App() {
-  const [completedTaskCount, setCompletedTaskCount] = useState(0);
-  const [todoLists, setTodoLists] = useState([]);
-  const [inputValues, setInputValues] = useState([]);
-  const [errors, setErrors] = useState([]);
-  const maxTodoLists = 6;
+function App () {
+  const [completedTaskCount, setCompletedTaskCount] = useState(0)
+  const [todoLists, setTodoLists] = useState([])
+  const [inputValues, setInputValues] = useState([])
+  const [errors, setErrors] = useState([])
+  const maxTodoLists = 6
 
-  const handleAddTask = (listIndex) => {
-    const inputValue = inputValues[listIndex].trim();
-    if (inputValue === "") {
-      setErrors((prevErrors) => {
-        const updatedErrors = [...prevErrors];
-        updatedErrors[listIndex] = "Task name cannot be empty.";
-        return updatedErrors;
-      });
-      return;
+  const handleAddTask = listIndex => {
+    const inputValue = inputValues[listIndex].trim()
+    if (inputValue === '') {
+      setErrors(prevErrors => {
+        const updatedErrors = [...prevErrors]
+        updatedErrors[listIndex] = 'Task name cannot be empty.'
+        return updatedErrors
+      })
+      return
     }
     // this is for non toched input field
-    setErrors((prevErrors) => {
-      const updatedErrors = [...prevErrors];
-      updatedErrors[listIndex] = ""; // Clear the error message for this list
-      return updatedErrors;
-    });
+    setErrors(prevErrors => {
+      const updatedErrors = [...prevErrors]
+      updatedErrors[listIndex] = '' // Clear the error message for this list
+      return updatedErrors
+    })
 
-    const id = todoLists[listIndex].tasks.length + 1;
+    const id = todoLists[listIndex].tasks.length + 1
     const newTask = {
       id: id,
       task: inputValue,
-      complete: false,
-    };
-    setTodoLists((prevLists) => {
-      const updatedTodoLists = [...prevLists];
+      complete: false
+    }
+    setTodoLists(prevLists => {
+      const updatedTodoLists = [...prevLists]
       updatedTodoLists[listIndex] = {
         ...updatedTodoLists[listIndex],
-        tasks: [...updatedTodoLists[listIndex].tasks, newTask],
-      };
-      return updatedTodoLists;
-    });
+        tasks: [...updatedTodoLists[listIndex].tasks, newTask]
+      }
+      return updatedTodoLists
+    })
 
     // Clear the input value for this list
-    setInputValues((prevInputValues) => {
-      const updatedInputValues = [...prevInputValues];
-      updatedInputValues[listIndex] = "";
-      return updatedInputValues;
-    });
-  };
+    setInputValues(prevInputValues => {
+      const updatedInputValues = [...prevInputValues]
+      updatedInputValues[listIndex] = ''
+      return updatedInputValues
+    })
+  }
 
   const handleComplete = (listIndex, taskId) => {
-    setTodoLists((prevLists) => {
-      const updatedTodoLists = [...prevLists];
-      const task = updatedTodoLists[listIndex].tasks.find((task) => task.id === taskId);
+    setTodoLists(prevLists => {
+      const updatedTodoLists = [...prevLists]
+      const task = updatedTodoLists[listIndex].tasks.find(
+        task => task.id === taskId
+      )
 
       if (task) {
         if (!task.complete) {
-          setCompletedTaskCount(completedTaskCount + 1);
+          setCompletedTaskCount(completedTaskCount + 1)
         } else {
-          setCompletedTaskCount(completedTaskCount - 1);
+          setCompletedTaskCount(completedTaskCount - 1)
         }
-        task.complete = !task.complete;
+        task.complete = !task.complete
       }
 
       updatedTodoLists[listIndex] = {
         ...updatedTodoLists[listIndex],
-        tasks: [...updatedTodoLists[listIndex].tasks],
-      };
-      updatedTodoLists[listIndex].tasks.sort((a, b) => (a.complete === b.complete ? 0 : a.complete ? -1 : 1));
-      return updatedTodoLists;
-    });
-  };
+        tasks: [...updatedTodoLists[listIndex].tasks]
+      }
+      updatedTodoLists[listIndex].tasks.sort((a, b) =>
+        a.complete === b.complete ? 0 : a.complete ? -1 : 1
+      )
+      return updatedTodoLists
+    })
+  }
 
-  const handleRemoveTodoList = (listIndex) => {
+  const handleRemoveTodoList = listIndex => {
     // Get the tasks of the list being removed
-    const tasksToRemove = todoLists[listIndex].tasks;
+    const tasksToRemove = todoLists[listIndex].tasks
 
-    setTodoLists((prevLists) => prevLists.filter((_, index) => index !== listIndex));
+    setTodoLists(prevLists =>
+      prevLists.filter((_, index) => index !== listIndex)
+    )
 
     // Update the completedTaskCount by subtracting the completed tasks in the removed list
-    setCompletedTaskCount((prevCount) => {
-      const completedTasksToRemove = tasksToRemove.filter((task) => task.complete).length;
-      return prevCount - completedTasksToRemove;
-    });
-  };
-
+    setCompletedTaskCount(prevCount => {
+      const completedTasksToRemove = tasksToRemove.filter(
+        task => task.complete
+      ).length
+      return prevCount - completedTasksToRemove
+    })
+  }
 
   const addTodoList = () => {
     if (todoLists.length >= maxTodoLists) {
-      alert(`You can only have up to ${maxTodoLists} todo lists.`);
-      return;
+      alert(`You can only have up to ${maxTodoLists} todo lists.`)
+      return
     }
 
-    const newListId = todoLists.length;
+    const newListId = todoLists.length
     const newTodoList = {
       id: newListId,
       name: `Todo List ${newListId + 1}`,
-      tasks: [],
-    };
-    setTodoLists((prevLists) => [...prevLists, newTodoList]);
+      tasks: []
+    }
+    setTodoLists(prevLists => [...prevLists, newTodoList])
 
     // Initialize the input value and error message for this list
-    setInputValues((prevInputValues) => [...prevInputValues, ""]);
-    setErrors((prevErrors) => [...prevErrors, ""]);
-  };
+    setInputValues(prevInputValues => [...prevInputValues, ''])
+    setErrors(prevErrors => [...prevErrors, ''])
+  }
 
   const handleInputChange = (listIndex, value) => {
-    setInputValues((prevInputValues) => {
-      const updatedInputValues = [...prevInputValues];
-      updatedInputValues[listIndex] = value;
-      return updatedInputValues;
-    });
-  };
+    setInputValues(prevInputValues => {
+      const updatedInputValues = [...prevInputValues]
+      updatedInputValues[listIndex] = value
+      return updatedInputValues
+    })
+  }
 
   return (
-    <div className="container">
-      <div className="header">
+    <div className='container'>
+      <div className='header'>
         <h2>Multiple Todo Lists</h2>
         {todoLists.length <= maxTodoLists && (
-          <button className="todoBtn" onClick={addTodoList}>Add Todo List</button>
+          <button className='todoBtn' onClick={addTodoList}>
+            Add Todo List
+          </button>
         )}
       </div>
-      <div className="todoLists">
+      <div className='todoLists'>
         {todoLists.map((todoList, listIndex) => (
-          <div className="todoCard" key={todoList.id}>
-            <div className="todoHeader">
-              <h3 style={{ fontSize: "18px" }}>{todoList.name}</h3>
-              <button style={{ background: "none", border: "none" }} onClick={() => handleRemoveTodoList(listIndex)}>
-                <a style={{ color: "lightsalmon", fontSize: "14px" }} href="#">Close</a>
+          <div className='todoCard' key={todoList.id}>
+            <div className='todoHeader'>
+              <h3 style={{ fontSize: '18px' }}>{todoList.name}</h3>
+              <button
+                style={{ background: 'none', border: 'none' }}
+                onClick={() => handleRemoveTodoList(listIndex)}
+              >
+                <a style={{ color: 'lightsalmon', fontSize: '14px' }} href='#'>
+                  Close
+                </a>
               </button>
             </div>
             <input
-              type="text"
+              type='text'
               value={inputValues[listIndex]}
-              placeholder="Enter a task name"
-              onChange={(e) => handleInputChange(listIndex, e.target.value)}
-              className="todoInput"
+              placeholder='Enter a task name'
+              onChange={e => handleInputChange(listIndex, e.target.value)}
+              className='todoInput'
             />
             <button
               onClick={() => handleAddTask(listIndex)}
-              className="todoTaskAdd"
+              className='todoTaskAdd'
             >
               + Add
             </button>
             <span>
-              {errors[listIndex] && <div style={{ color: "#ff5353" }}>{errors[listIndex]}</div>}
+              {errors[listIndex] && (
+                <div style={{ color: '#ff5353' }}>{errors[listIndex]}</div>
+              )}
             </span>
             <table>
               <thead>
@@ -155,25 +171,25 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                {todoList.tasks.map((task) => (
+                {todoList.tasks.map(task => (
                   <tr
                     key={task.id}
                     style={{
-                      textDecoration: task.complete ? "underline" : "none",
+                      textDecoration: task.complete ? 'underline' : 'none'
                     }}
                   >
                     <td>{task.id}</td>
                     <td>
                       <span
                         onClick={() => handleComplete(listIndex, task.id)}
-                        style={{ cursor: "pointer" }}
+                        style={{ cursor: 'pointer' }}
                       >
                         {task.task}
                       </span>
                     </td>
                     <td>
                       <input
-                        type="checkbox"
+                        type='checkbox'
                         checked={task.complete}
                         onChange={() => handleComplete(listIndex, task.id)}
                       />
@@ -186,12 +202,21 @@ function App() {
         ))}
       </div>
 
-      <div className="footer">
+      <div className='footer'>
         <div>Total Completed Tasks: {completedTaskCount}</div>
-        <div>Total Pending Tasks: {todoLists.reduce((total, list) => total + list.tasks.length - list.tasks.filter((task) => task.complete).length, 0)}</div>
+        <div>
+          Total Pending Tasks:{' '}
+          {todoLists.reduce(
+            (total, list) =>
+              total +
+              list.tasks.length -
+              list.tasks.filter(task => task.complete).length,
+            0
+          )}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App.js'
 // import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,9 +12,9 @@ import App from './App.js';
 // );
 
 class MywebsiteParent extends React.Component {
-  render() {
+  render () {
     return (
-      <div className="Cheem">
+      <div className='Cheem'>
         <App />
         {/* <h1>Hello Peter!</h1>
         <h3> {this.props.peterReaction}</h3> */}
@@ -24,43 +24,60 @@ class MywebsiteParent extends React.Component {
 }
 
 class MyWebsiteChild extends React.Component {
-  render() {
-    const Quote = "I'm a neighbourhood spider-man";
+  render () {
+    const Quote = "I'm a neighbourhood spider-man"
     return (
       <div>
         <MywebsiteParent peterReaction={Quote} />
-        <p>{this.props.message} Call me {this.props.hint}</p>
+        <p>
+          {this.props.message} Call me {this.props.hint}
+        </p>
       </div>
     )
   }
 }
 
-MyWebsiteChild.defaultProps = { message: "Hi I'm Spider-Man..", hint: "Spidey :)" }
+MyWebsiteChild.defaultProps = {
+  message: "Hi I'm Spider-Man..",
+  hint: 'Spidey :)'
+}
 
 class StateExample extends React.Component {
-  constructor() {
-    super();// readonly purpose of the super() to initialize the this.state value
-    this.state = { initialValue: "Please don't give me", objective: "Hope" }
+  constructor () {
+    super() // readonly purpose of the super() to initialize the this.state value
+    this.state = { initialValue: "Please don't give me", objective: 'Hope' }
   }
   changeValue = () => {
     const value = { name: "She's my" }
-    this.setState({ initialValue: value.name }); // this.setState only know the state is change by the user/interacter so need to re-render and update the changed value
+    this.setState({ initialValue: value.name }) // this.setState only know the state is change by the user/interacter so need to re-render and update the changed value
   }
-  render() {
+  render () {
     return (
       <div>
         <MyWebsiteChild /> <br />
         <h1>
           {this.state.initialValue} {this.state.objective}
         </h1>
-        <button style={{ padding: "10px", margin: "3px", border: "0.5px solid yellow", borderRadius: "6px", cursor: "pointer" }} type='button' onClick={this.changeValue}>Exit</button>
+        <button
+          style={{
+            padding: '10px',
+            margin: '3px',
+            border: '0.5px solid yellow',
+            borderRadius: '6px',
+            cursor: 'pointer'
+          }}
+          type='button'
+          onClick={this.changeValue}
+        >
+          Exit
+        </button>
       </div>
     )
   }
 }
 
 // ReactDOM.render(<MyWebsiteChild hint="Cool Avenger!"/>, document.getElementById('root')); // when we gave value it will appear otherwise default will appear
-ReactDOM.render(<MywebsiteParent />, document.getElementById('root'));
+ReactDOM.render(<MywebsiteParent />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
